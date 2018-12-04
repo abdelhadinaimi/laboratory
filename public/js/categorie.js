aa8cd3ccdfe1bbec947604c8ae59b5226d423b12
Saidi Boumediene <saidiboumi98@gmail.com>
2018-12-04 17:06:40 +0100

    Catégorie finished and side bar as component

---
+++ public/js/categorie.js
@@ -0,0 +1,100 @@
+    $(function () {
+           var manageCat = $("#gererCat").DataTable({
+            'ajax': 'getCat',
+             'order': []   
+             });
+           $("#submitCatForm").unbind('submit').bind('submit', function() {
+              var catLib = $("#catLib").val();
+              if(catLib == "") {
+                  $("#catLib").after('<p class="text-danger">Saissisz le libellé</p>');
+                  $('#catLib').closest('.form-group').addClass('has-error');
+              }
+              else{
+                var form = $(this);
+                $("#createCatBtn").button('loading');
+                $.ajax({
+                  url : form.attr('action'),
+                  type: form.attr('method'),
+                  data: form.serialize(),
+                  dataType: 'json',
+                  success:function(response) {
+                   $("#createCatBtn").button('reset');
+                     if(response.success == true) {
+                           manageCat.ajax.reload(null, false);
+                           $("#submitCatForm")[0].reset();
+                           $(".text-danger").remove();
+                           $('.form-group').removeClass('has-error').removeClass('has-success');
+                           $('#add-cat-messages').html('<div class="alert alert-success">'+
+            '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+            '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> '+ response.messages +
+          '</div>');
+                    $(".alert-success").delay(500).show(10, function() {
+                       $(this).delay(3000).hide(10, function() {
+                       $(this).remove();
+                        });
+                       }); // /.alert
+                    }  // if
+                  } // /success
+                  }); // /ajax
+               }
+              return false;
+           });
+
+        $('#removeCat').on('click',function(e){
+             var idCat = $("#getCatId").attr('role');
+              $.ajax({
+                url: 'deleteCat',
+                type: 'post',
+                dataType: 'json',
+                data: {"_token": $('meta[name="csrf-token"]').attr('content'),"idCat":idCat},
+                success:function(response) {
+                  $('#removeCatModal').modal('hide');
+                  manageCat.ajax.reload(null, false);
+                  $('.remove-messages').html('<div class="alert alert-success">'+
+                  '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+                  '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> Suppression Effectuée</div>');
+
+                  $(".alert-success").delay(500).show(10, function() {
+                    $(this).delay(3000).hide(10, function() {
+                          $(this).remove();
+                    });
+                  }); // /.alert
+                }
+             });
+        });
+        
+        $('#editCatBtn').on('click',function(e){
+             var idCat = $("#getCatId").attr('role');
+             var nvCatLib = $("#editCatName").val();
+             if(nvCatLib == ""){
+                $("#editCatName").after('<p class="text-danger">Saissisz le libellé</p>');
+                $('#editCatName').closest('.form-group').addClass('has-error');
+             }
+             else{
+              $.ajax({
+                url: 'editCat/'+idCat,
+                type: 'post',
+                dataType: 'json',
+                data: {"_token": $('meta[name="csrf-token"]').attr('content'),"catLib":nvCatLib},
+                success:function(response) {
+                  manageCat.ajax.reload(null, false);
+                           $("#editCatForm")[0].reset();
+                           $(".text-danger").remove();
+                           $('.form-group').removeClass('has-error').removeClass('has-success');
+                           $('#edit-cat-messages').html('<div class="alert alert-success">'+
+            '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+            '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> '+ response.message +
+          '</div>');
+                    $(".alert-success").delay(500).show(10, function() {
+                       $(this).delay(3000).hide(10, function() {
+                       $(this).remove();
+                        });
+                       }); // /.alert
+                }
+             });
+           }
+            return false;
+        });
+
+
+     });
