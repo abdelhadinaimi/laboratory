e86e148c02aea1c4edcd735883984993041d9a24
fodil <fredfawkes96@gmail.com>
2019-01-01 20:37:11 +0100

    fix bugs index

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -61,6 +61,14 @@
           </a>
         </li>
       @endif
+      @if(Auth::user()->role->nom == 'admin' )
+          <li class="{{ $active ==  'Stages' ? 'active':''}}">
+          <a href="{{url('stages')}}">
+            <i class="fa fa-folder-open-o"></i> 
+            <span>Stages</span>
+          </a>
+        </li>
+      @endif
         
       @if(Auth::user()->role->nom == 'admin' )
           <li class="{{ $active ==  'Messages' ? 'active':''}}">
